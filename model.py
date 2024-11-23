2084cb9388b3951cb61142c49c26fb4af7f87fda
George Giapitzakis <giorgosgiapis@mail.com>
2024-11-23 04:51:16 -0500

    fix

--- model.py
+++ model.py
@@ -139,12 +139,13 @@ class Transformer(nn.Module):
 
     def forward(self, x, p=None):
         #x = self.encoding(x)
+        device = next(self.parameters()).device
         nonneg_mask = x[:, :, 0] >= 0
         neg_mask = x[:, :, 0] < 0
         x_nonneg = torch.stack([x[i, nonneg_mask[i]] for i in range(x.size(0))])
         x_neg = torch.stack([x[i, neg_mask[i]] for i in range(x.size(0))])
-        emb = self.embedding(-x_neg[:,:,0].long())
-        lin = self.encoding(x_nonneg)
+        emb = self.embedding(-x_neg[:,:,0].long()).to(device)
+        lin = self.encoding(x_nonneg).to(device)
         x = torch.empty(x.size(0), x.size(1), self.embed_dim)
         for i in range(x.size(0)):
             x[i, nonneg_mask[i]] = lin[i]
